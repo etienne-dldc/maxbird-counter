4e356ecd10d21cc407a0bb5ac15ffe9c376cddaa
Etienne Dldc <e.deladonchamps@gmail.com>
2019-04-23 19:39:48 +0200

    Better style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { data } from './data';
 
 const App: React.FC = () => {
-  const [input, setInput] = React.useState<string>('');
+  const [input, setInput] = React.useState<string>('17, 18, 24, 25, 37, 49, 50, 52, 58, 59, 60, 67, 79, 92, 98');
 
   const nums = input
     .split(/\D+/g)
@@ -20,22 +20,44 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
+      <h1>Expedition Max Bird - Point counter</h1>
+      <p>Entrez vos numéros:</p>
       <input placeholder="17, 18, 24..." value={input} onChange={e => setInput(e.target.value)} />
-      <div>{nums.length} numéros</div>
-      <div>{nums.join(' - ')}</div>
-      <div>
+      <p>
+        {nums.length} {nums.length === 1 ? 'numéro' : 'numéros'}{' '}
+      </p>
+      <div className="nums">
+        {nums.map((num, index) => (
+          <span key={index} className="num">
+            {num}
+          </span>
+        ))}
+      </div>
+
+      <div className="score">
+        Score: <span className="points">{total} points</span>
+      </div>
+      <div className="items">
         {matching.map((piaf, index) => {
           if (!piaf) {
-            return <div key={index}>Invalid number</div>;
+            return (
+              <div className="item" key={index}>
+                Invalid number
+              </div>
+            );
           }
           return (
-            <div key={index} style={{ background: piaf.captured ? 'red' : 'white' }}>
-              {piaf.num} - {piaf.points} points
+            <div
+              className="item"
+              key={index}
+              style={{ background: piaf.captured ? '#E0E0E0' : 'white', opacity: piaf.captured ? 1 : 0.5 }}
+            >
+              <span className="num">{piaf.num}</span>
+              <span className="points">{piaf.points} points</span>
             </div>
           );
         })}
       </div>
-      <div>Total: {total} points</div>
     </div>
   );
 };
